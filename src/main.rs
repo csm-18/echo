4c41223786d339092e24b9be248f6a4aabddfce9
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-02-13 06:17:55 +0530

    cli handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+// echo version
+const VERSION:&str = "1.0.0";
 fn main() {
+    // cli args
     let args: Vec<String> = std::env::args().collect();
     let args = &args[1..];
-    println!("{:?}", args);
+    
+    // parse args
+    if args.is_empty() {
+        println!("echo v{VERSION}");
+        println!("To compile:");
+        println!(" echo <filename.echo>");
+    }else if args.len() == 1 {
+        println!("Compiling...");
+    }else {
+        println!("Too many arguments!");
+    }
 }
