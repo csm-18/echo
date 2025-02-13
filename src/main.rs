ad1e12e9ffa7338fb54ba1b10c4c85b207a25b41
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-02-13 06:33:33 +0530

    check source-file extension
    should be '.echo' else an error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::process::exit;
+
 // echo version
 const VERSION:&str = "1.0.0";
 fn main() {
@@ -11,7 +13,12 @@ fn main() {
         println!("To compile:");
         println!(" echo <filename.echo>");
     }else if args.len() == 1 {
-        println!("Compiling...");
+        let filename = &args[0];
+        //check filename extension
+        if filename.len() < 6 || &filename[filename.len()-5..] != ".echo" {
+            println!("Invalid source file!");
+            exit(1);
+        }
     }else {
         println!("Too many arguments!");
     }
