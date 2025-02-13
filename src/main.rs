ec8f3d0984f00ae6ad221f9414a1001117e77662
csm-18 <112937168+csm-18@users.noreply.github.com>
2025-02-13 07:04:48 +0530

    read source-file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::process::exit;
-
+use std::io::Read;
+use std::{path::Path, process::exit};
+use std::fs::File;
 // echo version
 const VERSION:&str = "1.0.0";
 fn main() {
@@ -14,11 +15,25 @@ fn main() {
         println!(" echo <filename.echo>");
     }else if args.len() == 1 {
         let filename = &args[0];
-        //check filename extension
+        // check filename extension
         if filename.len() < 6 || &filename[filename.len()-5..] != ".echo" {
             println!("Invalid source file!");
             exit(1);
         }
+
+        // source file path
+        let source_file_path = Path::new(filename);
+        
+        // open file
+        let mut source_file = match File::open(source_file_path){
+            Err(_err) => {println!("Unable to open '{filename}'");exit(1)},
+            Ok(file) => file,
+        };
+
+        // read file
+        let mut code = String::new();
+        if let Err(_err) = source_file.read_to_string(&mut code) {println!("Unable to read '{filename}'");exit(1);}
+        println!("{code}");
     }else {
         println!("Too many arguments!");
     }
